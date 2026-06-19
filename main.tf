120a82990a0a3d16d7f31036ca2a6c9806253d46
viyancs <msofyancs@gmail.com>
2026-06-19 09:46:17 +0700

    integrating aws to terraform and ansible

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ locals {
 #############################################
 
 data "aws_ssm_parameter" "ubuntu_2204" {
-  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp3/ami-id"
+  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
 }
 
 data "aws_caller_identity" "current" {}
